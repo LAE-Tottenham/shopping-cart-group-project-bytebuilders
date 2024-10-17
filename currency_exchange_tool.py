89d481174e9116bf908629efe595d0b471199be7
DanielStanecki <daniel.stanecki@laetottenham.ac.uk>
2024-10-17 09:12:11 +0000

    Pending changes exported from your codespace

--- currency_exchange_tool.py
+++ currency_exchange_tool.py
@@ -3,11 +3,44 @@ import math # you'll probably need this
 exchange_rates = {
     'USD': 1.13, #I.E. 1 Pound is 1.13 Dollars
     'EUR': 1.15,
+    'CAD' : 1.80,
+    'CHF' : 1.12,
+    'JPY': 194.43
 }
 
-def check_currency_exists(currency):
-    return
+def check_currency_exists():
+    currency = input("Please enter the currency that you would like to convert into: ").upper()
+    if currency in exchange_rates: 
+        return currency
+    else: 
+        return True
+        
 
-def currency_convert(original_c, new_c, amount):
+def currency_convert(currency):
     # your code here
-    return
+    original_amount = input("Please enter the amount that you'd like to convert: ")
+    while float(original_amount) < 10.0 or float(original_amount) > 1000.0:
+        original_amount = input("Please enter the amount that you'd like to convert: ") 
+
+    new_amount = float(original_amount) * exchange_rates[currency]
+    
+    
+    return round(new_amount, 2)
+
+print(exchange_rates)
+
+currency = check_currency_exists()
+
+while True: 
+    if currency == True: 
+        currency = check_currency_exists()
+    else:
+        break
+
+amount = str(currency_convert(currency))
+
+if amount[-2] == ".":
+    amount += "0"
+
+print("Your new amount in " + currency + " is " + str(amount))
+
