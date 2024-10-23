c2d908c61148fb1c1721163c783f66b5d53097a2
AlimMugalaasi <alim.mugalaasi@laetottenham.ac.uk>
2024-10-23 18:31:57 +0000

    Pending changes exported from your codespace

--- currency_exchange_tool.py
+++ currency_exchange_tool.py
@@ -1,13 +1,39 @@
-import math # you'll probably need this
 
-exchange_rates = {
-    'USD': 1.13, #I.E. 1 Pound is 1.13 Dollars
-    'EUR': 1.15,
-}
+#CURRENCY EXCHANGE FUCNCTION - DANIEL + VAHID
 
-def check_currency_exists(currency):
-    return
+def check_currency_exists(exchange_rates):
+    currency = input("Please enter the currency that you would like to convert into: ").upper()
+    if currency in exchange_rates: 
+        return currency
+    else: 
+        return True
 
-def currency_convert(original_c, new_c, amount):
-    # your code here
-    return
+def currency_convert(currency, exchange_rates, shoppingPrice):
+    original_amount = shoppingPrice
+    while float(original_amount) < 10.0 or float(original_amount) > 1000.0:
+        original_amount = input("Please enter the amount that you'd like to convert: ") 
+
+    new_amount = float(original_amount) * exchange_rates[currency]
+    
+    
+    return round(new_amount, 2)
+
+def view_rates(exchange_rates):
+    for value in exchange_rates:
+        print(f'{value}: '+ str("{:.2f}".format(exchange_rates[value])))
+
+def price_output(exchange_rates, shoppingPrice):
+    currency = check_currency_exists(exchange_rates)
+
+    while True: 
+        if currency == True: 
+            currency = check_currency_exists(exchange_rates)
+        else:
+            break
+
+    amount = str(currency_convert(currency, exchange_rates, shoppingPrice))
+
+    if amount[-2] == ".":
+        amount += "0"
+
+    return amount, currency
