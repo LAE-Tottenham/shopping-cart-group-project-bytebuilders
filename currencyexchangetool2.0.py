da72448a3c0116f6e59d1b44b8c6265daa9acaea
vahid-habib <vahid.habibzade@laetottenham.ac.uk>
2024-11-28 11:34:24 +0000

    message

--- currencyexchangetool2.0.py
+++ currencyexchangetool2.0.py
@@ -1,13 +1,24 @@
 import requests
-currency_dict = {
-    "GBP": 1,
-    "EUR": 1.20,
-    "USD": 1.31,
-    "CAD": 1.80,
-    "CHF": 1.12,
-    "JPY": 194.66
-}
-original_c = "GBP"
+
+url = 'https://marketdata.tradermade.com/api/v1/live?api_key=ceWoy_Kfrqq42dUehyOY&currency=GBPUSD,GBPEUR,GBPCHF,GBPJPY,GBPCAD'
+response = requests.get(url)
+currency_response = requests.get(url).json()["quotes"]
+
+if response.status_code == 200:
+
+        usd = response.json()["quotes"][0]["mid"]
+        eur = response.json()["quotes"][1]["mid"]
+        chf = response.json()["quotes"][2]["mid"]
+        jpy = response.json()["quotes"][3]["mid"]
+        cad = response.json()["quotes"][4]["mid"] 
+currency_dict={
+     "USD" : f"{usd}",
+     "EUR" : f"{eur}", 
+     "CAD" : f"{cad}", 
+     "CHF" : f"{chf}", 
+     "JPY" : f"{jpy}",  
+     }
+
 def check_currency_exists(currency):
 
  if currency in currency_dict:
@@ -25,6 +36,6 @@ def currency_convert():
          while float(amount) < 10.0 or float(amount) > 1000.0:
             print("amount must be > £10 and < £1000.")
             amount = float(input(f"What amount would you like to convert to {currency}: "))
-    convertedCurrency = amount*currency_dict[currency]
+    convertedCurrency = amount*float(currency_dict[currency])
     return f"{convertedCurrency:.2f}"
-print(currency_convert())   
+print(currency_convert())  
